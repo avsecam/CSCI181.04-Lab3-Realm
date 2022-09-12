cc33ac063660ed1807c4c7e7b8eb9e6a46770de1
Jed Jocson <jed.jocson@obf.ateneo.edu>
2022-09-12 23:40:42 +0800

    Add required functionality for LoginActivity.

--- app/src/main/java/com/avsecam/realm/LoginActivity.java
+++ app/src/main/java/com/avsecam/realm/LoginActivity.java
@@ -13,6 +13,8 @@ import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import io.realm.Realm;
+
 
 @EActivity(R.layout.activity_login)
 public class LoginActivity extends AppCompatActivity {
@@ -23,36 +25,32 @@ public class LoginActivity extends AppCompatActivity {
 
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
+    private Realm realm;
 
     @AfterViews
     protected void init() {
         sharedPreferences = getSharedPreferences(getString(R.string.SHAREDPREFERENCES_NAME), MODE_PRIVATE);
         editor = sharedPreferences.edit();
+        realm = Realm.getDefaultInstance();
 
         // Remember the remember me checkbox state
         if (sharedPreferences.contains(getString(R.string.REMEMBERME_KEY))) {
             rememberMeCheckBox.setChecked(sharedPreferences.getBoolean(getString(R.string.REMEMBERME_KEY), false));
         }
-        // Remember the saved credentials
-        if (checkForCredentials()) {
-            String savedUsername = sharedPreferences.getString(getString(R.string.USERNAME_KEY), "");
-            String savedPassword = sharedPreferences.getString(getString(R.string.PASSWORD_KEY), "");
-            usernameField.setText(savedUsername);
-            passwordField.setText(savedPassword);
-        }
     }
 
     @Click(R.id.buttonSignIn)
     public void onLoginButtonPressed() {
+        String username = usernameField.getText().toString();
+        String password = passwordField.getText().toString();
         // Check if there are already saved credentials
-        if (checkForCredentials()) {
-            String savedUsername = sharedPreferences.getString(getString(R.string.USERNAME_KEY), "");
-            String savedPassword = sharedPreferences.getString(getString(R.string.PASSWORD_KEY), "");
-            String username = usernameField.getText().toString();
-            String password = passwordField.getText().toString();
-            // Compare credentials
-            boolean correctCredentials = username.equals(savedUsername) && password.equals(savedPassword);
-            if (correctCredentials) {
+        if (checkForCredentials(username)) {
+            String savedPassword = realm.where(User.class).equalTo(getString(R.string.USERNAME_KEY), username).findFirst().getPassword();
+            // Compare password
+            if (password.equals(savedPassword)) {
+                // Save uuid to sharedPreferences
+                String savedUuid = realm.where(User.class).equalTo(getString(R.string.USERNAME_KEY), username).findFirst().getUuid();
+
                 // Go to landing page
                 Intent intent = new Intent(this, LandingActivity_.class);
                 startActivity(intent);
@@ -60,7 +58,7 @@ public class LoginActivity extends AppCompatActivity {
                 Toast.makeText(this, "Invalid credentials.", Toast.LENGTH_SHORT).show();
             }
         } else {
-            Toast.makeText(this, "Nothing saved.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "No User found.", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -72,12 +70,12 @@ public class LoginActivity extends AppCompatActivity {
 
     @Click(R.id.buttonClear)
     public void onClearButtonPressed() {
-        if (checkForCredentials()) {
+        if (sharedPreferences.contains(getString(R.string.UUID_KEY)) || sharedPreferences.contains(getString(R.string.REMEMBERME_KEY))) {
             editor.clear();
             editor.commit();
-            Toast.makeText(this, "Credentials cleared.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "SharedPreferences cleared.", Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(this, "No credentials to clear.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Nothing to clear.", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -88,7 +86,7 @@ public class LoginActivity extends AppCompatActivity {
         editor.commit();
     }
 
-    private boolean checkForCredentials() {
-        return sharedPreferences.contains(getString(R.string.USERNAME_KEY)) && sharedPreferences.contains(getString(R.string.PASSWORD_KEY));
+    private boolean checkForCredentials(String username) {
+        return realm.where(User.class).equalTo("name", username).findAll().size() == 1;
     }
 }
